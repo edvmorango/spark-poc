ea6cf8eaa2cebebfa8147d28d02a99150cae1241
J. Eduardo V. Morango <edvmorango@gmail.com>
2018-03-31 21:32:39 -0300

    clean

--- src/main/scala/config/SparkConfBuilder.scala
+++ src/main/scala/config/SparkConfBuilder.scala
@@ -5,17 +5,12 @@ import org.apache.spark.SparkConf
 object SparkConfBuilder {
 
   private val defConfig =
-    Map( "spark.app.name" -> "spark-poc",
-      "spark.master" -> "local",
-      "spark.driver.memory" -> "2g"
-    ).toTraversable
-
+    Map("spark.app.name" -> "spark-poc",
+        "spark.master" -> "local",
+        "spark.driver.memory" -> "2g")
 
   lazy val defaultConfiguration: SparkConf = {
     new SparkConf().setAll(defConfig)
   }
-
-
-
 
 }
--- src/main/scala/jobs/NasaRequestJob.scala
+++ src/main/scala/jobs/NasaRequestJob.scala
@@ -5,19 +5,84 @@ import org.apache.spark.rdd.RDD
 import utils.PathUtils
 import domain._
 
+import scala.collection.TraversableOnce
 import scala.util.Try
 
 case class NasaRequestJob(sc: SparkContext) {
 
-//  in24.inetnebr.com - - [01/Aug/1995:00:00:01 -0400] "GET /shuttle/missions/sts-68/news/sts-68-mcc-05.txt HTTP/1.0" 200 1839
+  // For some reason Sparks don't accept closures as HoF/methods, indepently of visibility level
+
+  private def transformData(l: String): NasaRequest = {
+    if (l.split(' ').size < 4)
+      NasaRequest("Ok", "", "", 200, 0)
+    else {
+
+      val host = l.split(' ').head
+
+      val timeHead = l.drop(host.size).dropWhile(_ != '[').tail.split(']')
+
+      val time = timeHead.head
+
+      val requestString = timeHead.tail.mkString("")
+
+      val reqClear = requestString.dropWhile(_ != '\"').replace("\"", "")
+
+      val reqList = reqClear.split(" ")
+
+      val reqParts = reqList.takeWhile(s => s.isEmpty || !s.forall(_.isDigit))
+
+      val request = reqParts.mkString(" ")
+
+      val cols = reqList.drop(reqParts.size)
+
+      val code = cols(0).toLong
+
+      val size = Try(cols(1).toLong).getOrElse(0L)
+
+      NasaRequest(host, time, request, code, size)
+    }
+  }
+
+  private def transformValidData(l: String): TraversableOnce[NasaRequest] = {
+    if (l.split(' ').size < 4)
+      Nil
+    else {
+
+      val host = l.split(' ').head
+
+      val timeHead = l.drop(host.size).dropWhile(_ != '[').tail.split(']')
+
+      val time = timeHead.head
+
+      val requestString = timeHead.tail.mkString("")
+
+      val reqClear = requestString.dropWhile(_ != '\"').replace("\"", "")
+
+      val reqList = reqClear.split(" ")
+
+      val reqParts = reqList.takeWhile(s => s.isEmpty || !s.forall(_.isDigit))
+
+      val request = reqParts.mkString(" ")
+
+      val cols = reqList.drop(reqParts.size)
+
+      val code = cols(0).toLong
+
+      val size = Try(cols(1).toLong).getOrElse(0L)
+
+      Array(NasaRequest(host, time, request, code, size))
+
+    }
+
+  }
 
   def execute() = {
 
     val raw: RDD[String] = sc.textFile(PathUtils.default)
 
-    val nrs: RDD[NasaRequest] = raw.map { l =>
+    val data: RDD[NasaRequest] = raw.flatMap { l =>
       if (l.split(' ').size < 4)
-        NasaRequest("Ok", "", "", 200, 0)
+        Nil
       else {
 
         val host = l.split(' ').head
@@ -42,11 +107,12 @@ case class NasaRequestJob(sc: SparkContext) {
 
         val size = Try(cols(1).toLong).getOrElse(0L)
 
-        NasaRequest(host, time, request, code, size)
+        Array(NasaRequest(host, time, request, code, size))
       }
+
     }
 
-    nrs
+    data
 
   }
 
